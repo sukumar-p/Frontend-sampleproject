f66b9ab331f3b99a540cc7ecd7a6702c758003b3
chanti123 <sukumar.p@mtwlabs.com>
2018-08-13 16:30:35 +0530

    loginpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
+import { RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes : Routes = ([
+{ path : 'login', component : LoginComponent}
+])
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
